17adf2be9bb007758b2f6567e2fd05598bdbf7ba
Abner Cruz <64279184+AbnerCruz@users.noreply.github.com>
2026-09-13 14:31:30 -0300

    Bump simulation version for Phase D

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,4 +1,4 @@
-export const VERSION=6;
+export const VERSION=7;
 export const MAP_W=192, MAP_H=192, TILE=16, MAX_NPCS=512, MAX_ANIMALS=900;
 export const DAY_TICKS=840;
 export const TECH_COUNT=36;
